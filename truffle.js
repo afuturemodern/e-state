5d6ee6996a5668470147e8bbdfad1fbca703f07d
Trevor C Martin <trevor@ddtechs.com>
2018-05-31 20:15:22 -0500

    setting up migrations for ropsten

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "shoe noodle tooth often spin share draft decade ancient blast drama keep";
+var infura_apikey = "0lahMLTdgVWYQPpNa8W8";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -7,6 +11,10 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       gasPrice: 2000000000,
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"+infura_apikey),
+      network_id: 3
     }
   }
 };
