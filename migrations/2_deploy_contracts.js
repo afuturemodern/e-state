7ce67229e7e71f988132b5cd6a1a93b653f13b9f
Trevor C Martin <trevor@ddtechs.com>
2018-06-01 10:52:29 -0500

    more synchronous - yes, the code is using async functions, but they're async by default, and I'm trying to chain them together synchronously...by using async and await....because that's what it's forcing me to do

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,39 +9,39 @@ module.exports = async function(deployer) {
   
   var token;
   var asset;
-  deployer.deploy(DeclaToken);
+  await deployer.deploy(DeclaToken);
   await DeclaToken.deployed().then(function(instance){token = instance;});
 
-  deployer.deploy(AssetToken, DeclaToken.address);
+  await deployer.deploy(AssetToken, DeclaToken.address);
   await AssetToken.deployed().then(function(instance){asset = instance;});
 
   token.setAssetContract(AssetToken.address);
 
-  deployer.deploy(Rentings, DeclaToken.address, AssetToken.address);
+  await deployer.deploy(Rentings, DeclaToken.address, AssetToken.address);
 
   token.setRentingsContract(Rentings.address);
 
   asset.setRentingsContract(Rentings.address);
 
-  deployer.deploy(CommunityContract, DeclaToken.address, AssetToken.address);
+  await deployer.deploy(CommunityContract, DeclaToken.address, AssetToken.address);
 
   token.setCommunityContract(CommunityContract.address);
 
   asset.setCommunityContract(CommunityContract.address);
 
-  deployer.deploy(CommentEconomy, DeclaToken.address, AssetToken.address);
+  await deployer.deploy(CommentEconomy, DeclaToken.address, AssetToken.address);
   token.setCommentContract(CommentEconomy.address);
 
   asset.setCommentContract(CommentEconomy.address);
 
-  deployer.deploy(
+  await deployer.deploy(
     IcoContract,
     '0x627306090abaB3A6e1400e9345bC60c78a8BEf57', //owner address
     DeclaToken.address,
     '1000000000000000000000000000',//1000000000 Token
     '1000',
     '1525791307', //05/08/2018
-    '1535068800', //sometime in august, idk
+    '1575068800', //sometime in august, idk
     '100000000000000000', //0.1 ETH
   );
 
